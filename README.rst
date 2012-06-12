24ccaca027b89bdb196aa7ee43fba8c887a33165
Christian Ledermann <christian.ledermann@gmail.com>
2012-06-12 20:05:02 +0300

    add description

---
+++ README.rst
@@ -0,0 +1,5 @@
+Introduction
+============
+
+Transform KML to HTML. It extracts the name and descrition from
+the placemarks and constructs a simple html document from this.
--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ version = '0.1'
 setup(name='collective.transform.kml',
       version=version,
       description="Transform KML to HTML",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
         "Programming Language :: Python",
+        "Topic :: Text Processing"
         ],
       keywords='',
       author='Christian Ledermann',
